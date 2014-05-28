87e791a84d678eaa62fd308892cff987c3676e8b
Jim Duey <jim@unknown28cfe918ea2d.gateway.pace.com>
2014-05-27 20:02:19 -0500

    Add test.applicative

---
+++ test/effects/test/applicative.clj
@@ -0,0 +1,8 @@
+(ns effects.test.applicative
+  (:refer-clojure :exclude [for extend])
+  (:require [clojure.test :refer :all]
+            [effects :refer :all]
+            [effects.free :refer :all])
+  (:import [effects.free Free Pure FreeA]))
+
+
