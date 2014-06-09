2bd1f74c64d591dc58fcb87eb64d3f12b04d614e
Jim Duey <jim@intensivesystems.net>
2014-06-08 19:16:12 -0500

    It parses!

--- test/effects/test/parser.clj
+++ test/effects/test/parser.clj
@@ -3,73 +3,136 @@
   (:require [effects :refer :all]
             [effects.free :refer :all]
             [effects.maybe :refer :all]
-            [effects.state :refer :all])
+            [effects.state :refer :all :exclude [set-val update-state]])
   (:import [effects.free Pure Free FreeA FreePlus FreeZero]))
 
-(def parser (plus state maybe))
-(defn parser-set-val [k v]
+(def state-maybe (plus state maybe))
+
+(defn update-state [f]
   (->State maybe
-           (fn [s] (maybe (list v (assoc s k v))))
-           (pr-str "parser-set-val" k v)))
+           (fn [s] (maybe (list s (f s))))
+           (pr-str "update-state")))
+
+(defn set-val [k v]
+  (update-state #(assoc % k v)))
 
 (deftype EBNF [x]
   Applicative
   (fapply* [_ args]
-    (EBNF. (fapply* (parser (fn [& args]
-                              (apply str (interpose " , " args))))
+    (EBNF. (fapply* (state-maybe (fn [& args]
+                                   (apply str (interpose " , " args))))
                     (map #(.x %) args))))
 
   Monoid
   (plus* [v vs]
-    (EBNF. (fapply* (parser (fn [& args]
-                              (apply str (interpose " | " args))))
+    (EBNF. (fapply* (state-maybe (fn [& args]
+                                   (apply str (interpose " | " args))))
                     (map #(.x %) (cons v vs))))))
 
 (defn term [v]
   (pure v))
 
+(defn pure-parser [term]
+  (if (fn? term)
+    (state-maybe term)
+    (for [[next-term] (update-state rest)
+          :when (= term next-term)]
+      [term])))
+
 (defn pure-ebnf [v]
-  (EBNF. (parser (condp = v
-                   \newline "\"\\n\""
-                   \tab "\"\\t\""
-                   \space "\" \""
-                   \formfeed "\"\\f\""
-                   (format "\"%s\"" v)))))
+  (EBNF. (state-maybe (condp = v
+                        \newline "\"\\n\""
+                        \tab "\"\\t\""
+                        \space "\" \""
+                        \formfeed "\"\\f\""
+                        (format "\"%s\"" v)))))
+(defprotocol GenParser
+  (gen-parser [_]))
 
 (defprotocol GenEBNF
   (gen-ebnf [_]))
 
-(deftype Rule [name expr]
-  EndoFunctor
-  (fmap [_ f] (Rule. name (fmap expr f)))
-
-  GenEBNF
-  (gen-ebnf [_]
-    (EBNF. (for [expr-ebnf (.x (evaluate expr pure-ebnf gen-ebnf))
-                 _ (parser-set-val name (str expr-ebnf " ;"))]
-             name))))
-
-(deftype NoneOrMore [expr]
-  EndoFunctor
-  (fmap [_ f] (NoneOrMore. (fmap expr f)))
-
-  GenEBNF
-  (gen-ebnf [_]
-    (EBNF. (for [expr-ebnf (.x (evaluate expr pure-ebnf gen-ebnf))]
-             (str "{ " expr-ebnf " }")))))
-
 (deftype Opt [expr]
   EndoFunctor
   (fmap [_ f] (Opt. (fmap expr f)))
+
+  GenParser
+  (gen-parser [_]
+    (plus (evaluate expr pure-parser gen-parser)
+          (state-maybe [])))
 
   GenEBNF
   (gen-ebnf [_]
     (EBNF. (for [expr-ebnf (.x (evaluate expr pure-ebnf gen-ebnf))]
              (str "[ " expr-ebnf " ]")))))
 
-(defmacro defrule [sym rule]
-  `(let [~sym (effects.free/free (->Rule '~sym (term "")))]
-     (def ~sym (effects.free/free (->Rule '~sym ~rule)))))
+(defn repeat-parser [parser]
+  (for [x parser
+        xs (plus (repeat-parser parser)
+                 (state-maybe []))]
+    (concat x xs)))
+
+(deftype NoneOrMore [expr]
+  EndoFunctor
+  (fmap [_ f] (OneOrMore. (fmap expr f)))
+
+  GenParser
+  (gen-parser [_]
+    (plus (repeat-parser (evaluate expr pure-parser gen-parser))
+          (state-maybe [])))
+
+  GenEBNF
+  (gen-ebnf [_]
+    (EBNF. (for [expr-ebnf (.x (evaluate expr pure-ebnf gen-ebnf))]
+             (str "{ " expr-ebnf " }")))))
+
+(deftype Rule [name expr handler-fn]
+  EndoFunctor
+  (fmap [_ f] (Rule. name (fmap expr f) handler-fn))
+
+  GenParser
+  (gen-parser [_]
+    (if expr
+      (for [parsed (evaluate expr pure-parser gen-parser)]
+        (if handler-fn
+          [(apply handler-fn parsed)]
+          parsed))
+      (for [parsed (evaluate (deref (resolve (symbol name)))
+                             pure-parser gen-parser)]
+        (if handler-fn
+          [(apply handler-fn parsed)]
+          parsed))))
+
+  GenEBNF
+  (gen-ebnf [_]
+    (if expr
+      (EBNF. (for [expr-ebnf (.x (evaluate expr pure-ebnf gen-ebnf))
+                   _ (set-val name (str expr-ebnf " ;"))]
+               name))
+      (EBNF. (state-maybe name)))))
+
+(deftype Ignore [expr]
+  EndoFunctor
+  (fmap [_ f] (Ignore. (fmap expr f)))
+
+  GenParser
+  (gen-parser [_]
+    (for [_ (evaluate expr pure-parser gen-parser)]
+      []))
+
+  GenEBNF
+  (gen-ebnf [_]
+    (evaluate expr pure-ebnf gen-ebnf)))
+
+(defmacro defrule
+  ([sym]
+     `(def ~sym (effects.free/free (->Rule '~sym nil nil))))
+  ([sym rule]
+     `(let [~sym (effects.free/free (->Rule '~sym nil nil))]
+        (def ~sym (effects.free/free (->Rule '~sym ~rule nil)))))
+  ([sym rule handler-fn]
+     `(let [~sym (effects.free/free (->Rule '~sym nil nil))]
+        (def ~sym (effects.free/free (->Rule '~sym ~rule ~handler-fn))))))
 
 (defn all [& parsers]
   (fapply* (pure concat) parsers))
@@ -78,10 +141,16 @@
 
 (def optional (comp free ->Opt))
 
-(def none-or-more (comp free ->NoneOrMore))
+(defn none-or-more [parser]
+  (free (->NoneOrMore parser)))
 
 (defn one-or-more [parser]
   (all parser (none-or-more parser)))
+
+(def ignore (comp free ->Ignore))
+
+(defn parser [expr]
+  (evaluate expr pure-parser gen-parser))
 
 
 
@@ -96,17 +165,34 @@
 (defrule upper-case (apply one-of (map term "ABCDEFGHIJKLMNOPQRSTUVWXYZ")))
 (defrule lower-case (apply one-of (map term "abcdefghijklmnopqrstuvwxyz")))
 (defrule letter (plus upper-case lower-case))
+
 (defrule integer (all (optional (one-of (term '+) (term '-)))
                       (apply one-of (map term "123456789"))
-                      (none-or-more (apply one-of (map term "0123456789")))))
+                      (none-or-more (apply one-of (map term "0123456789"))))
+  (fn [& digits]
+    (read-string (apply str digits))))
+
 (defrule whitespace (apply one-of (map term " \n\t\f")))
-(defrule word (one-or-more letter))
-(defrule form (one-of integer
-                      word
-                      (all (term \()
-                           (none-or-more form)
-                           (none-or-more whitespace)
-                           (term \)))))
+
+(defrule word
+  (one-or-more letter)
+  str)
+
+(defrule form)
+(defrule s-expr (all (ignore (term \())
+                     (none-or-more
+                      (all (ignore (none-or-more whitespace))
+                           (one-or-more form)))
+                     (ignore (term \))))
+  list)
+
+(defrule form
+  (one-of integer
+          word
+          s-expr))
 
 (println)
 (print-ebnf form)
+
+(println)
+(prn (extract ((parser form) "(str (add 15 92))")))
